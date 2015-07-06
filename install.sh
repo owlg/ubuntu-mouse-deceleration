706e99059fea12206c705c73e1b7bae1541ebadf
owlg <owlg@users.noreply.github.com>
2015-07-06 10:36:24 +0000

    Create install.sh
    Works for my Logitech Gaming 400s Mouse.

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xinput --set-prop "Logitech G400s Optical Gaming Mouse" "Device Accel Constant Deceleration" 2
+xinput --set-prop "Logitech G400s Optical Gaming Mouse" "Device Accel Velocity Scaling" 1
+xinput --set-prop "Logitech G400s Optical Gaming Mouse" "Device Accel Profile" -1
